b0efe11a096b541d377ce144f26318398453ebb5
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-06-13 13:18:44 +1200

    Update simplecov-lcov.rb

--- lib/simplecov-lcov.rb
+++ lib/simplecov-lcov.rb
@@ -82,7 +82,7 @@ module SimpleCov
       end
 
       def format_file(file)
-        filename = file.filename.gsub("#{SimpleCov.root}/", './')
+        filename = file.filename #.gsub("#{SimpleCov.root}/", './')
         "SF:#{filename}\n#{format_lines(file)}\nend_of_record\n"
       end
 
